691ac95a1e851e9037c9987ef60df5aa264ea37b
Luiz Guilherme Alvarez <luizgpa@gmail.com>
2024-10-22 10:56:07 -0300

    Spanish updated

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1742806786"
+    serviceWorkerVersion: "3357530643"
   }
 });
